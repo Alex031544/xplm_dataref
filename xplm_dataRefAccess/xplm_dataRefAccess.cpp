276a1cd220fcfde12c349b02036c098050168639
Alex031544 <alex.git@gmx.net>
2020-11-27 13:40:16 +0100

    finalise documentation

--- xplm_dataRefAccess/xplm_dataRefAccess.cpp
+++ xplm_dataRefAccess/xplm_dataRefAccess.cpp
@@ -1,48 +1,43 @@
 /**
- * @author:     Christopher Ruwisch
- * @version:    0.3
+ * @author:     Christopher Ruwisch, Alexander Behrens
+ * @version:    1.0
  * @brief:      Access class for XPlane plugin
- * @date:       2019/11/27
+ * @date:       2020/11/27
  */
 
 #include "xplm_dataRefAccess.h"
-#include "../common/errPrint.h"
-/**
- * [Xplm_dataRefAccess::Xplm_dataRefAccess description]
- */
-Xplm_dataRefAccess::Xplm_dataRefAccess() {
+#include "common/errPrint.h"
 
+
+Xplm_dataRefAccess::Xplm_dataRefAccess ()
+{
     // ADDING ELEMENTS TO VECTOR
     _refStorage.reserve(MAXSIZEREFS);
     _isInit = false;
     // CALL DESTRUCTOR IF NOT ENOUGH SPACE IS SET
-    if(_refStorage.capacity() != MAXSIZEREFS) {
+    if (_refStorage.capacity() != MAXSIZEREFS) {
         print_xplog("Could not reserve enought space");
     }
 
-	initStorage();
-}
-
-/**
- * [Xplm_dataRefAccess description]
- */
-Xplm_dataRefAccess::~Xplm_dataRefAccess() {
-    // DESTRUCTOR TASKS HERE
+    initStorage();
 }
 
 
-/**
- * [Xplm_dataRefAccess::initStorage description]
- * @return  [description]
- */
-bool Xplm_dataRefAccess::initStorage(void) {
+Xplm_dataRefAccess::~Xplm_dataRefAccess ()
+{
+}
+
+
+bool
+Xplm_dataRefAccess::initStorage (void)
+{
     // CHECK IF ALREADY INIT
-    if(!_isInit) {
+    if (!_isInit) {
 
-        for(uint32_t i = 0; i < MAXSIZEREFS; ++i) {
+        for (uint32_t i = 0; i < MAXSIZEREFS; ++i) {
             _refStorage[i] = XPLMFindDataRef(xplm_ref[i].refName);
-            if(_refStorage[i] == NULL) {
-                print_xplog("ID[%04d] ... %s wasn't found\n",i,xplm_ref[i].refName);
+            if (_refStorage[i] == NULL) {
+                print_xplog("ID[%04d] ... %s wasn't found\n", i, xplm_ref[i].refName);
             }
         }
         // SET INIT FLAG
@@ -51,287 +46,238 @@ bool Xplm_dataRefAccess::initStorage(void) {
     return _isInit;
 }
 
-// == READ FUNCTIONS ===========================================================
-/**
- * [readData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num, uint8_t *data) {
 
-
-    if(data == NULL) {
-        return;
-    }
-    else {
-        // To Do errorhandling
-        XPLMGetDatab(_refStorage[_num],data,0,getSize(_num));
-    }
-}
-
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num,
-                                  uint8_t *data,
-                                  int offSet,
-                                  int nElem)
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref, uint8_t *data)
 {
-    if(data == NULL) {
+    if (data == NULL) {
         return;
     }
     else {
-        XPLMGetDatab(_refStorage[_num],data,offSet,nElem);
+        // ToDo errorhandling
+        XPLMGetDatab(_refStorage[ref], data, 0, getSize(ref));
     }
 }
 
-/**
- * [Xplm_dataRefAccess::readData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num, int *data) {
 
-    if(data == NULL) {
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref,
+                              uint8_t *data,
+                              int offSet,
+                              int nElem)
+{
+    if (data == NULL) {
         return;
     }
     else {
-        // To Do errorhandling
-        if(getSize(_num) > 1) {
-            XPLMGetDatavi(_refStorage[_num],data,0,getSize(_num));
+        XPLMGetDatab(_refStorage[ref], data, offSet, nElem);
+    }
+}
+
+
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref, int *data)
+{
+    if (data == NULL) {
+        return;
+    }
+    else {
+        // ToDo errorhandling
+        if (getSize(ref) > 1) {
+            XPLMGetDatavi(_refStorage[ref], data, 0, getSize(ref));
         }
         else {
-            *data = XPLMGetDatai(_refStorage[_num]);
+            *data = XPLMGetDatai(_refStorage[ref]);
         }
     }
 }
 
-/**
- * [Xplm_dataRefAccess::readData description]
- * @param _num   [description]
- * @param data   [description]
- * @param offSet [description]
- * @param nElem  [description]
- */
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num,
-                                  int *data,
-                                  int offSet,
-                                  int nElem)
+
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref,
+                              int *data,
+                              int offSet,
+                              int nElem)
 {
-    if(data == NULL) {
+    if (data == NULL) {
         return;
     }
     else {
-        XPLMGetDatavi(_refStorage[_num],data,offSet,nElem);
+        XPLMGetDatavi(_refStorage[ref], data, offSet, nElem);
     }
 }
 
-/**
- * [Xplm_dataRefAccess::readData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num, float *data) {
 
-    if(data == NULL) {
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref, float *data)
+{
+
+    if (data == NULL) {
         return;
     }
     else {
-        // To Do errorhandling
-        if(getSize(_num) > 1) {
-            XPLMGetDatavf(_refStorage[_num],data,0,getSize(_num));
+        // ToDo errorhandling
+        if (getSize(ref) > 1) {
+            XPLMGetDatavf(_refStorage[ref], data, 0, getSize(ref));
         }
         else {
-            *data = XPLMGetDataf(_refStorage[_num]);
+            *data = XPLMGetDataf(_refStorage[ref]);
         }
     }
 }
 
-/**
- * [Xplm_dataRefAccess::readData description]
- * @param _num   [description]
- * @param data   [description]
- * @param offSet [description]
- * @param nElem  [description]
- */
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num,
-                                  float *data,
-                                  int offSet,
-                                  int nElem)
+
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref,
+                              float *data,
+                              int offSet,
+                              int nElem)
 {
-    if(data == NULL) {
+    if (data == NULL) {
         return;
     }
     else {
-        XPLMGetDatavf(_refStorage[_num],data,offSet,nElem);
+        XPLMGetDatavf(_refStorage[ref], data, offSet, nElem);
     }
 }
 
-/**
- * [Xplm_dataRefAccess::readData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::readData(enum xplm_ref_E _num, double *data) {
 
-    if(data == NULL) {
-        return;
-    }
-    else {
-        // To Do errorhandling
-        *data = XPLMGetDatad(_refStorage[_num]);
-    }
-}
-
-// == WRITE FUNCTIONS ==========================================================
-/**
- * [Xplm_dataRefAccess::writeData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num, uint8_t *data) {
-
-    if(data == NULL) {
-        return;
-    }
-    else {
-        // To Do errorhandling
-        XPLMSetDatab(_refStorage[_num],data,0,getSize(_num));
-    }
-}
-
-/**
- * [Xplm_dataRefAccess::writeData description]
- * @param _num   [description]
- * @param data   [description]
- * @param offSet [description]
- * @param nElem  [description]
- */
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num, uint8_t *data,int offSet, int nElem) {
-    if(data == NULL) {
-        return;
-    }
-    else {
-        XPLMSetDatab(_refStorage[_num],data,offSet,nElem);
-    }
-}
-
-/**
- * [Xplm_dataRefAccess::writeData description]
- * @param _num [description]
- * @param data [description]
- * @param off  [description]
- * @param n    [description]
- */
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num, int *data)
+void
+Xplm_dataRefAccess::readData (enum xplm_ref_e ref, double *data)
 {
-	if(data == NULL) {
-		return;
-	}
-	else {
-		// To Do errorhandling
-		if(getSize(_num) > 1) {
-			XPLMSetDatavi(_refStorage[_num], data, 0, getSize(_num));
-		}
-		else {
-			XPLMSetDatai(_refStorage[_num], *data);
-		}
-	}
+    if (data == NULL) {
+        return;
+    }
+    else {
+        // ToDo errorhandling
+        *data = XPLMGetDatad(_refStorage[ref]);
+    }
 }
 
-/**
- * [Xplm_dataRefAccess::writeData description]
- * @param _num   [description]
- * @param data   [description]
- * @param offSet [description]
- * @param nElem  [description]
- */
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num,
-                                   int *data,
-                                   int offSet,
-                                   int nElem)
+
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref, uint8_t *data)
 {
-    if(data == NULL) {
+    if (data == NULL) {
         return;
     }
     else {
-        XPLMSetDatavi(_refStorage[_num],data,offSet,nElem);
+        // ToDo errorhandling
+        XPLMSetDatab(_refStorage[ref], data, 0, getSize(ref));
     }
 }
 
-/**
- * [Xplm_dataRefAccess::writeData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num, float *data) {
 
-    if(data == NULL) {
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref, uint8_t *data, int offSet, int nElem)
+{
+    if (data == NULL) {
         return;
     }
     else {
-        // To Do errorhandling
-        if(getSize(_num) > 1) {
-            XPLMSetDatavf(_refStorage[_num],data,0,getSize(_num));
+        XPLMSetDatab(_refStorage[ref], data, offSet, nElem);
+    }
+}
+
+
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref, int *data)
+{
+    if (data == NULL) {
+        return;
+    }
+    else {
+        // ToDo errorhandling
+        if (getSize(ref) > 1) {
+            XPLMSetDatavi(_refStorage[ref], data, 0, getSize(ref));
         }
         else {
-            XPLMSetDataf(_refStorage[_num],*data);
+            XPLMSetDatai(_refStorage[ref], *data);
         }
     }
 }
 
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num,
-                                   float *data,
-                                   int offSet,
-                                   int nElem)
+
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref,
+                               int *data,
+                               int offSet,
+                               int nElem)
 {
-    if(data == NULL) {
+    if (data == NULL) {
         return;
     }
     else {
-        XPLMSetDatavf(_refStorage[_num],data,offSet,nElem);
+        XPLMSetDatavi(_refStorage[ref], data, offSet, nElem);
     }
 }
 
-/**
- * [Xplm_dataRefAccess::writeData description]
- * @param _num [description]
- * @param data [description]
- */
-void Xplm_dataRefAccess::writeData(enum xplm_ref_E _num, double *data) {
 
-    if(data == NULL) {
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref, float *data)
+{
+    if (data == NULL) {
         return;
     }
     else {
-        // To Do errorhandling
-        XPLMSetDatad(_refStorage[_num],*data);
+        // ToDo errorhandling
+        if (getSize(ref) > 1) {
+            XPLMSetDatavf(_refStorage[ref], data, 0, getSize(ref));
+        }
+        else {
+            XPLMSetDataf(_refStorage[ref], *data);
+        }
     }
 }
 
-/**
- * [Xplm_dataRefAccess::isWritableRef description]
- * @param  _num [description]
- * @return      [description]
- */
-bool Xplm_dataRefAccess::isWritableRef(enum xplm_ref_E _num) {
-    if(xplm_ref[_num].isWritable) {
+
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref,
+                               float *data,
+                               int offSet,
+                               int nElem)
+{
+    if (data == NULL) {
+        return;
+    }
+    else {
+        XPLMSetDatavf(_refStorage[ref], data, offSet, nElem);
+    }
+}
+
+
+void
+Xplm_dataRefAccess::writeData (enum xplm_ref_e ref, double *data)
+{
+    if (data == NULL) {
+        return;
+    }
+    else {
+        // ToDo errorhandling
+        XPLMSetDatad(_refStorage[ref], *data);
+    }
+}
+
+
+bool
+Xplm_dataRefAccess::isWritableRef (enum xplm_ref_e ref)
+{
+    if (xplm_ref[ref].isWritable) {
         return true;
     }
     return false;
 }
 
-/**
- * [Xplm_dataRefAccess::getType description]
- * @param  _num [description]
- * @return      [description]
- */
-int Xplm_dataRefAccess::getType(enum xplm_ref_E _num) {
-    return xplm_ref[_num].type;
+
+xplm_types_t
+Xplm_dataRefAccess::getType (enum xplm_ref_e ref)
+{
+    return xplm_ref[ref].type;
 }
 
-/**
- * [Xplm_dataRefAccess::getSize description]
- * @param  _num [description]
- * @return      [description]
- */
-int Xplm_dataRefAccess::getSize(enum xplm_ref_E _num) {
-    return xplm_ref[_num].size;
+
+int
+Xplm_dataRefAccess::getSize (enum xplm_ref_e ref)
+{
+    return xplm_ref[ref].size;
 }
--- xplm_dataRefAccess/xplm_dataRefAccess.h
+++ xplm_dataRefAccess/xplm_dataRefAccess.h
@@ -1,70 +1,181 @@
 /**
- * @author:     Christopher Ruwisch
- * @version:    0.3
+ * @author:     Christopher Ruwisch, Alexander Behrens
+ * @version:    1.0
  * @brief:      Access class for XPlane plugin
- * @date:       2019/11/27
+ * @date:       2020/11/27
  */
 
-#ifndef _XPLM_DATAREFACCESS_H
-#define _XPLM_DATAREFACCESS_H
+#ifndef XPLM_DATAREFACCESS_H_INCLUDED
+#define XPLM_DATAREFACCESS_H_INCLUDED
 
-// == SYSTEM INCLUDES
+// standard includes
 #include <cstdint>
 #include <vector>
-// == XPLANE SDK INCLUDES
+// XPLANE SDK includes
 #include "XPLM/XPLMDataAccess.h"
 
-// == OWN INCLUDES
 #include "xplm_dataRef.h"
 
 
-// == CLASS DEFINITIONS
-
-/**
- * Xplm_dataRefAccess Class
- * @brief: Reserving space + R/W of Variables via ENUM
+/** @brief: Xplm_dataRefAccess Class
+ *
+ * Reserving space + R/W of Variables via ENUM
  */
 class Xplm_dataRefAccess
 {
 public:
-    // == CONSTRUCTOR / DESTRUCTOR ==
-    Xplm_dataRefAccess();
-    ~Xplm_dataRefAccess();
+    /** @brief Constructor
+     */
+    Xplm_dataRefAccess ();
 
-    // == PUBLIC ATTRIBUTES ==
+    /** @brief Destructor
+     */
+    ~Xplm_dataRefAccess ();
 
-    // == PUBLIC METHODS ==
-    bool initStorage(void);
+    /** @brief initialise datarefs (singleton)
+     *
+     * @return true if successful otherwise false
+     */
+    bool initStorage (void);
 
-	// GETTING DATA
-	void readData(enum xplm_ref_E _num, uint8_t *data);
-    void readData(enum xplm_ref_E _num, uint8_t *data, int offSet, int nElem);
-	void readData(enum xplm_ref_E _num, int *data);
-    void readData(enum xplm_ref_E _num, int *data, int offSet, int nElem);
-	void readData(enum xplm_ref_E _num, float *data);
-    void readData(enum xplm_ref_E _num, float *data, int offSet, int nElem);
-	void readData(enum xplm_ref_E _num, double *data);
+    /** @brief Read data of type uint8_t from XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  store the data to the buffer pointed to by data
+     */
+    void readData (enum xplm_ref_e ref, uint8_t *data);
 
-	// SETTING DATA
-	void writeData(enum xplm_ref_E _num, uint8_t *data);
-    void writeData(enum xplm_ref_E _num, uint8_t *data, int offSet, int nElem);
-	void writeData(enum xplm_ref_E _num, int *data);
-    void writeData(enum xplm_ref_E _num, int *data, int offSet, int nElem);
-	void writeData(enum xplm_ref_E _num, float *data);
-    void writeData(enum xplm_ref_E _num, float *data, int offSet, int nElem);
-	void writeData(enum xplm_ref_E _num, double *data);
+    /** @brief Read data of type uint8_t[] from XPlane internal memory
+     *
+     * @param ref     Dataref key
+     * @param data    store the data to the buffer pointed to by data
+     * @param offSet  offset where to begin copying data from source array
+     * @param nElem   number of elements to copy beginning from the offset
+     */
+    void readData (enum xplm_ref_e ref, uint8_t *data, int offSet, int nElem);
 
-    bool isWritableRef(enum xplm_ref_E _num);
-    int getType(enum xplm_ref_E _num);
-    int getSize(enum xplm_ref_E _num);
+    /** @brief Read data of type int from XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  store the data to the buffer pointed to by data
+     */
+    void readData (enum xplm_ref_e ref, int *data);
+
+    /** @brief Read data of type int[] from XPlane internal memory
+     *
+     * @param ref     Dataref key
+     * @param data    store the data to the buffer pointed to by data
+     * @param offSet  offset where to begin copying data from source array
+     * @param nElem   number of elements to copy beginning from the offset
+     */
+    void readData (enum xplm_ref_e ref, int *data, int offSet, int nElem);
+
+    /** @brief Read data of type float from XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  store the data to the buffer pointed to by data
+     */
+    void readData (enum xplm_ref_e ref, float *data);
+
+    /** @brief Read data of type float[] from XPlane internal memory
+     *
+     * @param ref     Dataref key
+     * @param data    store the data to the buffer pointed to by data
+     * @param offSet  offset where to begin copying data from source array
+     * @param nElem   number of elements to copy beginning from the offset
+     */
+    void readData (enum xplm_ref_e ref, float *data, int offSet, int nElem);
+
+    /** @brief Read data of type double from XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  store the data to the buffer pointed to by data
+     */
+    void readData (enum xplm_ref_e ref, double *data);
+
+    /** @brief Write data of type uint8_t to the XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  take the data from the buffer pointed to by data
+     */
+    void writeData (enum xplm_ref_e ref, uint8_t *data);
+
+    /** @brief Write data of type uint8_t[] to the XPlane internal memory
+     *
+     * @param ref     Dataref key
+     * @param data    take the data from the buffer pointed to by data
+     * @param offSet  offset where to begin copying data to the source array
+     * @param nElem   number of elements to copy beginning from the offset
+     */
+    void writeData (enum xplm_ref_e ref, uint8_t *data, int offSet, int nElem);
+
+    /** @brief Write data of type int to the XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  take the data from the buffer pointed to by data
+     */
+    void writeData (enum xplm_ref_e ref, int *data);
+
+    /** @brief Write data of type int[] to the XPlane internal memory
+     *
+     * @param ref     Dataref key
+     * @param data    take the data from the buffer pointed to by data
+     * @param offSet  offset where to begin copying data to the source array
+     * @param nElem   number of elements to copy beginning from the offset
+     */
+    void writeData (enum xplm_ref_e ref, int *data, int offSet, int nElem);
+
+    /** @brief Write data of type float to the XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  take the data from the buffer pointed to by data
+     */
+    void writeData (enum xplm_ref_e ref, float *data);
+
+    /** @brief Write data of type float[] to the XPlane internal memory
+     *
+     * @param ref     Dataref key
+     * @param data    take the data from the buffer pointed to by data
+     * @param offSet  offset where to begin copying data to the source array
+     * @param nElem   number of elements to copy beginning from the offset
+     */
+    void writeData (enum xplm_ref_e ref, float *data, int offSet, int nElem);
+
+    /** @brief Write data of type double to the XPlane internal memory
+     *
+     * @param ref   Dataref key
+     * @param data  take the data from the buffer pointed to by data
+     */
+    void writeData (enum xplm_ref_e ref, double *data);
+
+    /** @brief Informs if the field of XPlane memory is writable or not
+     * 
+     * @param ref   Dataref key
+     * @return      true if writeable or false otherwise
+     */
+    bool isWritableRef (enum xplm_ref_e ref);
+
+    /** @brief Informs about the data type a field of XPlane memory have
+     * 
+     * @param ref   Dataref key
+     * @return      The datatype represented by enum type xplm_types_t
+     */
+    xplm_types_t getType (enum xplm_ref_e ref);
+
+    /** @brief Informs about the data size a field of XPlane memory have
+     * 
+     * @param ref   Dataref key
+     * @return      The size of the field. A size greater than 1 indicates an array.
+     */
+    int getSize (enum xplm_ref_e ref);
+
 private:
-    // == PRIVATE ATTRIBUTES ==
-    std::vector<XPLMDataRef>    _refStorage;
-    bool                        _isInit;
+    /** @brief a vector containing all pointers to the fields in the XPlane memory */
+    std::vector <XPLMDataRef> _refStorage;
 
-
-
+    /** @brief keeps the information if the datarefs are already initialised */
+    bool _isInit;
 };
 
 
-#endif // _XPLM_DATAREFACCESS_H
+#endif // XPLM_DATAREFACCESS_H_INCLUDED
